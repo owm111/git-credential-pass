f7cc172eee1f310ecdc52d66ac48a65970e343a3
Owen McGrath <7798336+owm111@users.noreply.github.com>
2020-04-30 10:44:01 -0500

    remove nix expression

--- default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.stdenv.mkDerivation {
-  pname = "sh-pass-git-helper";
-  version = "0.0.0";
-  src = ./.;
-  installPhase = "install -Dvm 755 pass-git-helper $out/bin/pass-git-helper";
-}
